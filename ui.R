b9546a9753b274ec441b8557da6ec6097aab5582
Yao Xu <34262576+yx1441@users.noreply.github.com>
2022-05-13 09:21:54 +0400

    Initial commit

--- ui.R
+++ ui.R
@@ -10,38 +10,11 @@
 # source("global.R")
 #install.packages("radiant.data")
 #install.packages("radiant", repos = "https://radiant-rstats.github.io/minicran/")
-library(radiant.data)
 library(shiny)
 library(tidyverse);library(devtools);library(readtext);library(xtable);library(janitor);library(cowplot);library(survey);library(ggmap)
 library(ggplot2);library(reshape2);library(data.table);library(openxlsx);library(rlang);library(ggpubr);library(rgdal);library(spdep)
 library(maptools);library(tigris);library(leaflet);library(sf);library(maps);library(janitor);library(censusxy);library(dplyr);library(rgeos)
 library(geosphere); require(dplyr) 
-
-load_url <- function (url, ..., sha1 = NULL) {
-    # based very closely on code for devtools::source_url
-    stopifnot(is.character(url), length(url) == 1)
-    temp_file <- tempfile()
-    on.exit(unlink(temp_file))
-    request <- httr::GET(url)
-    httr::stop_for_status(request)
-    writeBin(httr::content(request, type = "raw"), temp_file)
-    file_sha1 <- digest::digest(file = temp_file, algo = "sha1")
-    if (is.null(sha1)) {
-        message("SHA-1 hash of file is ", file_sha1)
-    }
-    else {
-        if (nchar(sha1) < 6) {
-            stop("Supplied SHA-1 hash is too short (must be at least 6 characters)")
-        }
-        file_sha1 <- substr(file_sha1, 1, nchar(sha1))
-        if (!identical(file_sha1, sha1)) {
-            stop("SHA-1 hash of downloaded file (", file_sha1, 
-                 ")\n  does not match expected value (", sha1, 
-                 ")", call. = FALSE)
-        }
-    }
-    load(temp_file, envir = .GlobalEnv)
-}
 
 githubURL<-"https://github.com/yx1441/Discrimination_Reporting/blob/32862e023bc5a74444680df80720b9ab8c82d659/reporting_test_20220512.RData?raw=true"
 load(url(githubURL))
@@ -87,27 +60,4 @@ ui <- fluidPage(
                #mainPanel(plotOutput("distPlot")),
                ))
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    output$basisPlot <- renderPlot({
-        if (input$year=="2014-2019"){
-            time_series(input$basis,year_full)+ylim(0,100)
-        }
-        if (input$year=="2015-2018"){
-            time_series(input$basis,year_4)+ylim(0,65)
-        }
-    })
-    output$harmPlot <- renderPlot({
-        if (input$year=="2014-2019"){
-            time_series(input$harm,year_full)#+ylim(0,65)
-        }
-        if (input$year=="2015-2018"){
-            time_series(input$harm,year_4)#+ylim(0,65)
-        }
-    })
-    
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
 
