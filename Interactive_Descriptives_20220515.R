a078a3a8c8d6981ab288596cb2e9b17bd1dcf5a4
Yao Xu <34262576+yx1441@users.noreply.github.com>
2022-05-15 18:17:22 +0400

    updated co-occurences

--- Interactive_Descriptives_20220515.R
+++ Interactive_Descriptives_20220515.R
@@ -3,21 +3,70 @@
 # Interactive Descriptives
 # 5/15/22
 
+# RUN FROM REPO
 # runGitHub(repo = 'Discrimination_Reporting',username ='yx1441')
 
+# LOAD PACKAGES ----------------------
 library(shiny);library(tidyverse);library(devtools);library(readtext);library(xtable);library(janitor);library(cowplot)
 library(ggplot2);library(reshape2);library(data.table);library(openxlsx);library(rlang);library(ggpubr)
 library(leaflet);library(janitor);library(dplyr);library(corrplot)
 
+# PREPARATION----------------------
 githubURL<-"https://github.com/yx1441/Discrimination_Reporting/blob/32862e023bc5a74444680df80720b9ab8c82d659/reporting_test_20220512.RData?raw=true"
 load(url(githubURL))
 reporting_test<- reporting_test %>% dplyr::mutate(record_type2=case_when(reporting_test$record_type=="Employment" ~ "Complaint Only",
                                                             reporting_test$record_type=="Right to Sue" ~ "Right to Sue Only"))
+
+reporting_test<-reporting_test %>% dplyr::rename(har_denied_envir_free_of_discrim=har_denied_a_work_environment_free_of_discrimination_or_retaliation,
+                                                 har_asked_impermissible_questions=har_asked_impermissible_nonjobrelated_questions,
+                                                 har_denied_accommodation_disabilit=har_denied_reasonable_accommodation_for_a_disability,
+                                                 har_denied_employment_benefit=har_denied_any_employment_benefit_or_privilege,
+                                                 har_subjected_to_violence=har_subjected_to_a_threat_of_violence_or_violence_to_self_or_property,
+                                                 har_denied_good_faith_process=har_denied_a_good_faith_interactive_process,
+                                                 har_unequal_considerations_in_empl_decisions=har_failed_to_give_equal_considerations_in_making_employment_decisions)
+
+ba_all <-colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")]
+har_all <-colnames(reporting_test)[str_detect(colnames(reporting_test),"har_")]
+y_axis<-c("Percentage","Count")
+c_type<-c("Full","Complaint Only","Right to Sue Only")
+unit_choice<-c("By ISO weeks","By months", "By quarters", "By years")
 year_full<-c(2014:2019)
 year_4<-c(2015:2018)
-
 year_choice<-c("2015-2018","2014-2019")
+dat_empl<-subset(reporting_test[reporting_test$record_type=="Employment",])
+dat_rts<-subset(reporting_test[reporting_test$record_type=="Right to Sue",])
+reporting_test_1518<-reporting_test[reporting_test$complaint_year %in% c(2015:2018),]
+dat_empl_1518<-subset(dat_empl[dat_empl$record_type=="Employment",])
+dat_rts_1518<-subset(dat_rts[dat_rts$record_type=="Right to Sue",])
 
+c <- reporting_test %>% rowwise() %>% mutate(sum = sum(across(starts_with("ba_")), na.rm = T))
+
+num_co<-data.frame(count=seq(1:21)-1)
+for (i in 1:length(colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")])){
+    data<-NULL
+    data<-data.frame(table(c$sum[c[[colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")][i]]]==1]-1))
+    colnames(data)<-c("count",colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")][i])
+    data[[paste0(colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")][i],"_pct")]]<-data[[colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")][i]]]/sum(reporting_test[[colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")][i]]])
+    num_co<-merge(num_co,data,by=c("count"),all.x=T)
+}
+
+sum(num_co$ba_disabilitypct,na.rm=T)
+cooc_full<-as.data.frame(row_to_names(transpose(num_co,keep.names="rn"),1,remove_row = T))
+# print(xtable(see[,1:12]), include.rownames=FALSE)
+
+
+# Check data inconsistencies w.r.t. annual reports from DFEH
+nrow(reporting_test[reporting_test$complaint_year==2019&reporting_test$record_type=="Employment",])
+nrow(reporting_test[reporting_test$complaint_year==2017&reporting_test$record_type=="Employment",])
+nrow(reporting_test[reporting_test$complaint_year==2018&reporting_test$record_type=="Employment",])
+nrow(reporting_test[reporting_test$complaint_year==2016&reporting_test$record_type=="Employment",])
+nrow(reporting_test[reporting_test$complaint_year==2016,])
+
+nrow(reporting_test[reporting_test$complaint_year==2015&reporting_test$record_type=="Employment",])
+nrow(reporting_test[reporting_test$complaint_year==2015,])
+
+
+# FUNCTIONS --------------
 time_series<-function(reporting_test, ba_category, year, y_axis, harmorbasis){
     if (harmorbasis=="Basis"){
         if (y_axis=="Percentage"){
@@ -69,7 +118,6 @@ time_series<-function(reporting_test, ba_category, year, y_axis, harmorbasis){
         axis.line = element_line(colour = "black"))+ labs(fill = "Complaint Year")+ylim(0,1200)
    }}
     }
-
 overall_time_series<-function(year, record_type2, time_unit){
     if (time_unit=="By quarters"){
         if (record_type2=="Full"){
@@ -165,10 +213,7 @@ overall_time_series<-function(year, record_type2, time_unit){
         }
     }
 }
-
-corrplot2 <- function(data, tl.pos="d", method = "pearson",sig.level = 0.05, order = "original",diag = FALSE, type = "upper",tl.srt = 90,number.font = 1,number.cex = 1,mar = c(0, 0, 0, 0)) {
-    data_incomplete <- data
-    data <- data[complete.cases(data), ]
+corrplot2 <- function(data,method = "pearson",sig.level = 0.05, order = "original",diag = FALSE, type = "upper",tl.srt = 90,number.font = 1,number.cex = 1,mar = c(0, 0, 0, 0)) {
     mat <- cor(data, method = method,use = "complete.obs")
     cor.mtest <- function(mat, method) {
         mat <- as.matrix(mat)
@@ -197,15 +242,7 @@ corrplot2 <- function(data, tl.pos="d", method = "pearson",sig.level = 0.05, ord
     )
 }
 
-ba_all <-colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")]
-har_all <-colnames(reporting_test)[str_detect(colnames(reporting_test),"har_")]
-y_axis<-c("Percentage","Count")
-c_type<-c("Full","Complaint Only","Right to Sue Only")
-unit_choice<-c("By ISO weeks","By months", "By quarters", "By years")
-
-#mainPanel(plotOutput("basisPlot")),
-?get
-
+# UI  --------------
 ui <- fluidPage(
     navbarPage("Employment Discrimination Reporting",
                tabPanel("Times Series",
@@ -249,14 +286,35 @@ ui <- fluidPage(
                         ),
                tabPanel("Correlations",
                         tabsetPanel(
-                            tabPanel("Bases", br(),
-                                     selectInput("ba_cor","Select bases to create correlation matrices",
-                                    ba_all, multiple = TRUE, width = "50%"), # selectInput("year", "Select which years to visualize", choices = year_choice)
-                                    plotOutput(outputId = "bacorPlot")),
-                            tabPanel("Harms", br(),
+                            tabPanel("Bases Only", br(),
+                                     selectInput("ba_cor","Select bases to create correlation matrices", ba_all, multiple = TRUE, width = "75%"),
+                                     selectInput("year_cor", "Select which years to visualize", choices = year_choice),
+                                     radioButtons(
+                                        "complaint_type_cor", "Select a case type: ", 
+                                        choices = c_type, 
+                                        inline = TRUE),
+                                    
+                                    plotOutput(outputId = "bacorPlot", width = "100%")),
+
+                            tabPanel("Harms Only", br(),
                                      selectInput("har_cor","Select harms to create correlation matrices",
-                                                 har_all, multiple = TRUE, width = "50%"), # selectInput("year", "Select which years to visualize", choices = year_choice)
-                                     plotOutput(outputId = "harcorPlot")),
+                                                 har_all, multiple = TRUE, width = "75%"),
+                                     selectInput("year_cor2", "Select which years to visualize", choices = year_choice),
+                                     radioButtons(
+                                         "complaint_type_cor2", "Select a case type: ", 
+                                         choices = c_type, 
+                                         inline = TRUE),
+                                     plotOutput(outputId = "harcorPlot", width = "100%")),
+                            tabPanel("Bases and Harms", br(),
+                                     selectInput("har_cor2","Select harms to create correlation matrices", har_all, multiple = TRUE, width = "75%"),
+                                     selectInput("ba_cor2","Select bases to create correlation matrices", ba_all, multiple = TRUE, width = "75%"),
+                                     # selectInput("year_cor", "Select which years to visualize", choices = year_choice),
+                                     # radioButtons(
+                                     #     "complaint_type_cor", "Select a case type: ", 
+                                     #     choices = c_type, 
+                                     #    inline = TRUE),
+                                     plotOutput(outputId = "cocorPlot", width = "100%")
+                                     ),
                         
                         )
                         ),
@@ -267,26 +325,61 @@ ui <- fluidPage(
                         ),
                #mainPanel(plotOutput("distPlot")),
                ))
-nrow(reporting_test[reporting_test$complaint_year==2019&reporting_test$record_type=="Employment",])
-nrow(reporting_test[reporting_test$complaint_year==2017&reporting_test$record_type=="Employment",])
-nrow(reporting_test[reporting_test$complaint_year==2018&reporting_test$record_type=="Employment",])
-nrow(reporting_test[reporting_test$complaint_year==2016&reporting_test$record_type=="Employment",])
-nrow(reporting_test[reporting_test$complaint_year==2016,])
 
-nrow(reporting_test[reporting_test$complaint_year==2015&reporting_test$record_type=="Employment",])
-nrow(reporting_test[reporting_test$complaint_year==2015,])
-
-#new_empl<-dat_empl[,1:20]
-#dat_empl<-subset(reporting_test[reporting_test$record_type=="Employment",], select=colnames(reporting_test)[str_detect(colnames(reporting_test),"ba_")])
-
-# overall_time_series(2015,"Complaint Only","By months")
-# Define server logic required to draw a histogram
+# plotOutput(outputId = "bacorPlot", width = "100%")
+# SERVER  --------------
 server <- function(input, output) {
+    output$cocorPlot<- renderPlot({ 
+        corrplot2(data=reporting_test[,c(input$ba_cor2,input$har_cor2)], order = "original", diag = FALSE, type = "upper")
+        })
     output$bacorPlot <- renderPlot({
-        corrplot2(data=reporting_test[,input$ba_cor], order = "original", diag = FALSE, type = "upper")
+        if (input$complaint_type_cor=="Full"){
+            if (input$year_cor=="2014-2019"){
+                corrplot2(data=reporting_test[,input$ba_cor], order = "original", diag = FALSE, type = "upper") 
+            }
+            else{
+                corrplot2(data=reporting_test_1518[,input$ba_cor], order = "original", diag = FALSE, type = "upper") 
+            }
+        }
+        else if(input$complaint_type_cor=="Complaint Only"){
+            if (input$year_cor=="2014-2019"){
+                corrplot2(data=dat_empl[,input$ba_cor], order = "original", diag = FALSE, type = "upper")
+            }
+            else{
+                corrplot2(data=dat_empl_1518[,input$ba_cor], order = "original", diag = FALSE, type = "upper") 
+            }
+        }
+        else if(input$complaint_type_cor=="Right to Sue Only"){
+            if (input$year_cor=="2014-2019"){
+                corrplot2(data=dat_rts[,input$ba_cor], order = "original", diag = FALSE, type = "upper")
+            }
+            else{
+                corrplot2(data=dat_rts_1518[,input$ba_cor], order = "original", diag = FALSE, type = "upper")
+            }
+        }
     })
     output$harcorPlot <- renderPlot({
-        corrplot2(data=reporting_test[,input$har_cor], order = "original", diag = FALSE, type = "upper")
+        if (input$complaint_type_cor2=="Full"){
+            if (input$year_cor2=="2014-2019"){
+                corrplot2(data=reporting_test[,input$har_cor],order = "original", diag = FALSE, type = "upper") 
+            }else{
+                corrplot2(data=reporting_test_1518[,input$har_cor], order = "original", diag = FALSE, type = "upper") 
+            }
+        }
+        else if(input$complaint_type_cor2=="Complaint Only"){
+            if (input$year_cor2=="2014-2019"){
+                corrplot2(data=dat_empl[,input$har_cor], order = "original", diag = FALSE, type = "upper")
+            }else{
+                corrplot2(data=dat_empl_1518[,input$har_cor], order = "original", diag = FALSE, type = "upper") 
+            }
+        }
+        else if(input$complaint_type_cor2=="Right to Sue Only"){
+            if (input$year_cor2=="2014-2019"){
+                corrplot2(data=dat_rts[,input$har_cor], order = "original", diag = FALSE, type = "upper")
+            }else{
+                corrplot2(data=dat_rts_1518[,input$har_cor], order = "original", diag = FALSE, type = "upper")
+            }
+        }
     })
     output$tab1_text <- renderText({"Employment complaints filed by law in 2017 is 4,346; 2018 4,216 (DFEH, 2017, p.9)
         15,832 FOR 2016, PAGE 8 (Of the total complaints received by the Department, 17,041 complaints were formally filed by DFEH in 2016. 
@@ -357,6 +450,6 @@ server <- function(input, output) {
     
 }
 
-# Run the application 
+# RUN FROM LOCAL----------------
 shinyApp(ui = ui, server = server)
 
